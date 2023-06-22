21cbb1686e0e33d904934077757e08308a3dc052
Aviral <aviral.saxena.11.70@gmail.com>
2023-06-22 16:16:02 +0530

    first-commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import ReactDOM  from 'react-dom';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render(<h1>Noice!!</h1>,document.getElementById("root"));
+
+// ReactDOM.render(
+//   [<p>Hello </p>,<h3>How are you</h3>,<h4>I am fine</h4>]
+// ,document.getElementById("root"))
+
+// ReactDOM.render(
+//   <React.fr>
+//   <h1>Hello</h1>
+//   <h2>How are you</h2>
+//   </React.fr>,
+//   document.getElementById("root")
+// )
+
+// ReactDOM.render(
+//   <>
+//   <h1>Hello</h1>
+//   <h2>How are you</h2>
+//   </>,
+//   document.getElementById("root")
+// )
+
+//! using expressions in reactjs
+//todo: just add {}
+
+// var x="kya hal chal";
+
+// ReactDOM.render(
+//   <>
+//     <h2>How are you ...{x}</h2>
+//     <h1>My lucky number is {2+3}</h1>
+//     <h1>Random no generator {Math.floor(Math.random()*10)}</h1>
+//   </>,
+//   document.getElementById("root")
+// )
+
+//! templeate literals
+
+// var x="kya hal chal";
+// var y='sahi ha bhai';
+
+// const dt=new Date().toLocaleDateString();
+// const tm=new Date().toLocaleTimeString();
+
+// ReactDOM.render(
+//   <>
+//     <h2>How are you ...{`${x}${y}`}</h2>
+//     <h1>Today date is : {dt}</h1>
+//     <h1>Today time is : {tm}</h1>
+//   </>,
+//   document.getElementById("root")
+// )
+
+var name="aviral";
+
+ReactDOM.render(
+  <h1 contentEditable="true">My name is {name}</h1>,
+  document.getElementById("root")
+)
